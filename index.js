9b233970cf4d7caeb1118f8f8ff2667c38d14d6e
Yannis Becker <yannisbecker.dev@gmail.com>
2021-07-04 20:02:53 +0200

    fix defaults (#7)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import md2anki from './src/index.js';
-import { program } from 'commander/esm.mjs';
+import { program, Option } from 'commander/esm.mjs';
 import { createRequire } from 'module';
 
 const { version } = createRequire(import.meta.url)('./package.json'); // some magic from StackOverflow https://stackoverflow.com/a/56350495/6792590
 program.version(version);
 
 program
-	.option('-i, --input <path>', 'markdown file path', './README.md')
-	.option('-o, --output <path>', 'apkg file path', './output.apkg')
-	.option('-n, --deck-name <name>', 'name of the deck', 'md2anki')
+	.addOption(new Option('-i, --input <path>', 'markdown file path').default('./README.md'))
+	.addOption(new Option('-o, --output <path>', 'apkg file path').default('./output.apkg'))
+	.addOption(new Option('-n, --deck-name <name>', 'name of the deck').default('md2anki'))
 	.option('-e, --include-empty', 'include empty cards in the deck')
 	.parse(process.argv);
 
