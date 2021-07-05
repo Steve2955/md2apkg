e49cc4dc0791eccbcc14b8dc7fb172899338ca0a
Oliver Kogel <kogel.oliver@web.de>
2021-07-05 23:13:21 +0200

    implement support for images
    it finally works!

--- src/index.js
+++ src/index.js
@@ -77,15 +77,24 @@ export function deckFromCards(cards, images, options) {
 	console.log(`deck initialized!`);
 	// add media files to deck
 	images.forEach(image => {
-		apkg.addMedia(image, fs.readFileSync(image));
+		apkg.addMedia(image.replace('/', '#'), fs.readFileSync(image));
 	});
 	// add cards to deck (convert tokens to html)
 	cards.forEach(card => {
-		apkg.addCard(md.renderer.render(card.front, md.options, {}), md.renderer.render(card.back, md.options, {}));
+		apkg.addCard(md.renderer.render(card.front, md.options, {}), fixImagePaths(images, md.renderer.render(card.back, md.options, {})));
 	});
 	console.log(`added ${cards.length} cards to the deck!`);
 	console.log(`added ${images.length} images to the deck!`);
 	return apkg;
+}
+
+// make sure all images references point to the correct image file (/ replaced with #)
+export function fixImagePaths(images, html) {
+	images.forEach(image => {
+		html = html.replace(image, image.replace('/', '#'));
+	});
+
+	return html;
 }
 
 export function imagesFromTokens(tokens, inputPath) {
@@ -94,6 +103,7 @@ export function imagesFromTokens(tokens, inputPath) {
 		if (token.type === 'image') {
 			// find images in tokens and save them to a list
 			let filePath = path.join(path.dirname(inputPath), token.attrGet('src'));
+			// replace \ with / (in case we're on a windows system)
 			filePath = filePath.split(path.sep).join(path.posix.sep);
 			images.push(filePath);
 		}
@@ -106,6 +116,9 @@ export function imagesFromTokens(tokens, inputPath) {
 
 	// flatten the list to a depth of 1
 	images = images.flat();
+	// this process produces a lot of empty entries, remove them
+	images = images.filter(image => image.length > 0);
 
-	return images.filter(image => image.length > 0);
+	// only return unique images
+	return [...new Set(images)];
 }
