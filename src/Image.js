6e30ae55c8ab5aae156ce1655d27b2f95b6c58fc
Oliver Kogel <kogel.oliver@web.de>
2021-07-08 19:19:49 +0200

    Images
    replace *ever* slash

--- src/Image.js
+++ src/Image.js
@@ -2,6 +2,6 @@ export default class Image {
 	// filePath -> relative path to the file
 	constructor(filePath) {
 		this.filePath = filePath;
-		this.filteredPath = filePath.replace('/', '#');
+		this.filteredPath = filePath.replace(/\//g, '#');
 	}
 }
